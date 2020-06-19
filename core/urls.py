2f3409218926e6f492b574ddda8c4c80ad5cd9a6
scooredmars <olo880@spoko.pl>
2020-06-19 14:34:44 +0200

    reverse when chabge password

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from sfbot.views import MyPasswordChangeView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(("sfbot.urls", "sfbot"))),
+    path("password/change/", MyPasswordChangeView.as_view(),
+         name="account_change_password"),
     path("", include("allauth.urls")),
 ]
