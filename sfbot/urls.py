d3197ff8f22cecd38d9f115942bde6097bef566b
scooredmars <olo880@spoko.pl>
2020-09-17 15:36:12 +0200

    New toasts

--- sfbot/urls.py
+++ sfbot/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
     path("", views.Home.as_view(), name="index"),
-    path("dashboard", views.DashboardAddBot, name="dashboard"),
+    path("dashboard", views.dashboard_add_bot_view, name="dashboard"),
     path("<int:pk>", views.SettingsView.as_view(), name="settings"),
     path("<int:pk>", views.UserBotDetails.as_view(), name="bot-detail"),
     path("edit/<int:pk>", views.EditBotDetails.as_view(), name="edit-bot"),
-    path("profile", views.ProfileView, name="profile"),
-    path("shop", views.shop, name="shop"),
+    path("profile", views.profile_view, name="profile"),
+    path("shop", views.shop_view, name="shop"),
     path("faq", views.Faq.as_view(), name="faq"),
     path("regulations", views.Regulations.as_view(), name="regulations"),
     path("privacy-policy", views.PrivacyPolicy.as_view(), name="privacy-policy"),
